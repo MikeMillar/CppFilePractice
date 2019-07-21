2fc328a71c7672a1ecf25967f46ef4f875f60ec5
Mike Millar <39285355+MikeMillar@users.noreply.github.com>
2019-07-20 19:38:41 -0600

    parsing files

---
+++ CppFilePractice/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	// Writing to file
+	//ofstream outFile;
+
+	//string outputFileName = "text.txt";
+
+	//outFile.open(outputFileName);
+
+	//if (outFile.is_open()) {
+	//	for (int i = 1; i < 11; i++) {
+	//		outFile << i << ". Hello There." << endl;
+	//	}
+	//	outFile.close();
+	//}
+	//else {
+	//	cout << "Could not create file: " << outputFileName << endl;
+	//}
+
+
+	// Reading from file
+	string inFileName = "text.txt";
+	ifstream inFile;
+	/*
+	inFile.open(inFileName);
+
+	if (inFile.is_open()) {
+		string line;
+
+		while (inFile) {
+			getline(inFile, line);
+			cout << line << endl;
+		}		
+
+		inFile.close();
+	}
+	else {
+		cout << "Cannot open file: " << inFileName << endl;
+	}*/
+
+
+	inFile.open(inFileName);
+	if (!inFile.is_open()) {
+		return 1;
+	}
+
+	while (inFile) {
+		string line;
+		getline(inFile, line, ':');
+
+		int pop;
+		inFile >> pop;
+
+		//inFile.get();
+		inFile >> ws;
+
+		cout << "'" << line << "'" << "---" << "'" << pop << "'" << endl;
+
+		if (!inFile) {
+			break;
+		}
+	}
+
+	inFile.close();
+
+	return 0;
+}
